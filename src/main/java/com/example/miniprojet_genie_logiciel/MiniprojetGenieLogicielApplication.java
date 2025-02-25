4172ae351a83174125deb5ffb973818ede5b76ad
hamza-lzr <lazaarhamza@gmail.com>
2025-02-25 15:57:30 +0000

    first commit

---
+++ src/main/java/com/example/miniprojet_genie_logiciel/MiniprojetGenieLogicielApplication.java
@@ -0,0 +1,13 @@
+package com.example.miniprojet_genie_logiciel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniprojetGenieLogicielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniprojetGenieLogicielApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/miniprojet_genie_logiciel/MiniprojetGenieLogicielApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miniprojet_genie_logiciel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniprojetGenieLogicielApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
